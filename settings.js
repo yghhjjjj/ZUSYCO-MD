4e77f51dace1c4075a169702be1cb3a0a022ff5a
yghhjjjj <157750235+yghhjjjj@users.noreply.github.com>
2024-04-03 12:30:01 +0200

    settings.js

--- settings.js
+++ settings.js
@@ -16,39 +16,39 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NUMBER: "201095499866",
+  SUDO_NUMBERS : "201095499866",
+  OWNER_NAME: "『 S N A R K O 』",
+  BOT_NAME : "S N A R K O",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
-    ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
-    WELCOME: 'https://i.ibb.co/PzhpBV7/WELCOME-IMAGE-LOWQ.jpg',
-    GOODBYE: 'https://i.ibb.co/t8hKpcM/GOODBYE-IMAGE-LOWQ.jpg'
+    ALIVE: 'https://telegra.ph/file/ea14da6e3d042c039df38.jpg',
+    WELCOME: 'https://telegra.ph/file/ea14da6e3d042c039df38.jpg',
+    GOODBYE: 'https://telegra.ph/file/ea14da6e3d042c039df38.jpg'
   },
   STOP_SEND : {
         WELCOME_MESSAGE : false,
         GOODBYE_MESSAGE : false
     },
   DESABLE_WELCOME_GOODBYE_JIDS : [],
-  TIME_ZONE :  "Asia/Colombo",
-  WORK_TYPE : "public",
+  TIME_ZONE :  "Africa/Cairo",
+  WORK_TYPE : "private",
   WORK_MODE : "onlygroups",
-  WORK_MODE_MESSAGE_SEND : false,
-  CAPTION : '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ᴢᴜꜱʏᴄᴏ ᴍᴅ ʙᴏᴛ',
-  STICKER_PACK : '[ ZUSYCO ]',
+  WORK_MODE_MESSAGE_SEND : true,
+  CAPTION : '©S N A R K O',
+  STICKER_PACK : '[ S N A R K O ]',
   BAD_WORDS : 'fuck,pussy',
   ANTI_BAD : false,
   ANTI_LINKS : 'chat.whatsapp.com',
-  ANTI_LINK : false,
-  ANTI_LINK_KICK : false,
+  ANTI_LINK : true,
+  ANTI_LINK_KICK : true,
   MAX_SIZE : 100,
   MOROCCO_NUMBERS : {
         BLOCK : false,
         NOT_WORKING : false,
   },
   READ : {
-     ALL_MESSAGES : true,
+     ALL_MESSAGES : false,
      ONLY_COMMANDS : false,
      STATUS : false,
   },
